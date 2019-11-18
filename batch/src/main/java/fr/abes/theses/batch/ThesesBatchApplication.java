b6510dd84e571459185ade2122995989f82ef3c5
Julien Gibert <gibert@abes.fr>
2019-11-18 10:31:20 +0100

    [Fix] ServiceProvider bean wasn't found. @ComponentScan added.

--- batch/src/main/java/fr/abes/theses/batch/ThesesBatchApplication.java
+++ batch/src/main/java/fr/abes/theses/batch/ThesesBatchApplication.java
@@ -2,8 +2,10 @@ package fr.abes.theses.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages ={"fr.abes.theses.*"})
 public class ThesesBatchApplication {
 
 	public static void main(String[] args) {
