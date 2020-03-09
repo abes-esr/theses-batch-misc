2f30a5cb0ca70c75c2ef945f8ab8db1c5f40f006
le-maire <le-maire@abes.fr>
2020-03-09 13:43:01 +0100

    Update AccesCBS version

--- batch/src/main/resources/sh/rediffusionThese.sh
+++ batch/src/main/resources/sh/rediffusionThese.sh
@@ -2,4 +2,4 @@
 
 cd /home/batch/thesesBatch/current
 
-java /usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar batch.jar --server.port=8080 --idThese=20122 > ./logs/batch_single.log &
+java /usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar batch.jar --server.port=8080 --idThese=$1 > ./logs/batch_single.log &
