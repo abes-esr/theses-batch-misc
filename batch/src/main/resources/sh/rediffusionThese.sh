88562aff6e414b7d5b4072e8209a9cc1f8480868
le-maire <le-maire@abes.fr>
2020-03-12 11:16:15 +0100

    FIX : change rediffusionThese.sh

--- batch/src/main/resources/sh/rediffusionThese.sh
+++ batch/src/main/resources/sh/rediffusionThese.sh
@@ -2,4 +2,4 @@
 
 cd /home/batch/thesesBatch/current
 
-java /usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --idThese=$1 > ../logs/batch_single.log &
+/usr/java/jdk11/bin/java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --idThese=$1 >> ../logs/batch_single.log &
