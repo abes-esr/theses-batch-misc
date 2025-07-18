0e960c274170f6c0399e82cb7ce9047c86f7f962
TristanReamot <tristan.reamot@gmail.com>
2025-07-18 11:20:26 +0200

    fix: sortie des logs du batch dans la sortie standard docker

--- batch/src/main/resources/sh/rediffusionThese.sh
+++ batch/src/main/resources/sh/rediffusionThese.sh
@@ -2,4 +2,4 @@
 
 cd /home/batch/thesesBatch/current
 
-java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --idThese=$1 >> ../logs/batch_single.log &
+java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --idThese=$1 >> 1>/proc/1/fd/1 2>/proc/1/fd/2
