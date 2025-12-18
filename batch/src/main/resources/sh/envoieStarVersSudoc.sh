629a12c981e628d949645193e912f8a9bf1e0fb5
le-maire <le-maire@abes.fr>
2025-12-18 15:37:12 +0100

    Update: redirection des logs de thesesBatch vers batch_single.log

--- batch/src/main/resources/sh/envoieStarVersSudoc.sh
+++ batch/src/main/resources/sh/envoieStarVersSudoc.sh
@@ -3,5 +3,5 @@
 cd /home/batch/thesesBatch/current
 
 if [[ $(pgrep -cf "thesesBatch.jar --batch_bdd") = 0 ]];
-java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --batch_bdd=true 1>/proc/1/fd/1 2>/proc/1/fd/2
+java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --batch_bdd=true >> ../logs/batch_single.log
 if
