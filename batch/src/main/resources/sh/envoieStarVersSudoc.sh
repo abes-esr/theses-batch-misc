583905c5640fc44507d04c833344a0cb78d615b4
le-maire <le-maire@abes.fr>
2025-12-18 15:46:11 +0100

    fix: ajout du fi manquant dans envoieStarVersSudoc.sh

--- batch/src/main/resources/sh/envoieStarVersSudoc.sh
+++ batch/src/main/resources/sh/envoieStarVersSudoc.sh
@@ -1,7 +1,7 @@
-
+#!/bin/bash
 
 cd /home/batch/thesesBatch/current
 
 if [[ $(pgrep -cf "thesesBatch.jar --batch_bdd") = 0 ]];
 java -Djava.security.egd=file:///dev/urandom -jar thesesBatch.jar --batch_bdd=true >> ../logs/batch_single.log
-if
+fi
